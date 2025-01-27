aeb78a24bdbf43353330ad7b9fee30a323d12ae7
damianozaccaron <damianozaccaron@gmail.com>
2025-01-28 00:05:25 +0100

    Per oggi apposto

--- data_processor/forecast_sarima.py
+++ data_processor/forecast_sarima.py
@@ -2,14 +2,20 @@ from datetime import datetime, timedelta
 import pandas as pd
 import pickle
 
-def get_prediction_for_week(parking_id, start_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S"), festive_dates = None, zone = None):
+def get_prediction_for_week(parking_id, zone, start_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S"), festive_dates = None):
     """returns forecast for the next week given a parking_id and start_time. """ 
+    VALID_ZONES = ["NORD", "SUD", "CENTRO", "EST", "OVEST"]
+    
+    if zone not in VALID_ZONES:
+        raise ValueError(f"Invalid zone: {zone}. Valid zones are {VALID_ZONES}.")
+    
     try:
         with open("app/data_processor/sarima_model.pkl", "rb") as f:
             loaded_model = pickle.load(f)
     except Exception as e:
         print(f"Error loading model, be sure to train it first: {e}")
 
+    
     new_records = []
     for i in range(168):
         new_records.append({
@@ -20,6 +26,7 @@ def get_prediction_for_week(parking_id, start_time = datetime.now().strftime("%Y
     if festive_dates is None:
         festive_dates = get_festive_dates()
 
+
     df_aggregated = pd.DataFrame(new_records)
     df_aggregated['timestamp'] = pd.to_datetime(df_aggregated['timestamp'])
 
@@ -27,29 +34,27 @@ def get_prediction_for_week(parking_id, start_time = datetime.now().strftime("%Y
     df_aggregated['day_of_week'] = df_aggregated['timestamp'].dt.dayofweek  # 0=Monday, 6=Sunday
     df_aggregated['is_festive'] = df_aggregated['day_of_week'].apply(lambda x: 1 if x == 6 else 0)  # Sunday is festive
     df_aggregated['is_festive'] = df_aggregated['timestamp'].dt.date.isin(pd.to_datetime(festive_dates).date) | df_aggregated['is_festive']
+    df_aggregated['zone'] = zone
 
-    # Create dummies
-    df_aggregated['parking_id'] = df_aggregated['parking_id'].astype(str) 
-    parking_dummies = pd.get_dummies(df_aggregated['parking_id'], prefix='parking')
-    df_aggregated = df_aggregated.join(parking_dummies).drop(columns=['parking_id', 'timestamp'])
-    
-    if zone is not None:
-        df_aggregated['zone'] = zone
-        zone_dummies = pd.get_dummies(df_aggregated['zone'], prefix='zone')
-        df_aggregated = df_aggregated.join(zone_dummies).drop(columns=['zone'])
-    
+    # Create dummy columns for each zone
+    for valid_zone in VALID_ZONES:
+        df_aggregated[f'zone_{valid_zone}'] = (df_aggregated['zone'] == valid_zone).astype(int)
+
+    df_aggregated.drop(columns=['zone', 'timestamp', 'parking_id'], inplace=True)
+
     df_aggregated = df_aggregated.astype(int)
     df_aggregated = df_aggregated.apply(pd.to_numeric, errors='coerce').fillna(df_aggregated.mean())
 
+    # Forecast using the loaded SARIMA model
     forecast = loaded_model.forecast(steps=168, exog=df_aggregated)
 
     forecast_df = pd.DataFrame({
-    "parking_id": parking_id,
-    "timestamp": pd.date_range(start=start_time, periods=168, freq="h"),
-    "forecasted_occupancy": forecast
+        "parking_id": parking_id,
+        "timestamp": pd.date_range(start=start_time, periods=168, freq="h"),
+        "forecasted_occupancy": forecast
     })
 
-    return forecast_df # pandas dataframe with forecasted occupancy for the next week
+    return forecast_df  # pandas dataframe with forecasted occupancy for the next week
 
 
 def get_festive_dates():
--- data_processor/train_sarima.py
+++ data_processor/train_sarima.py
@@ -5,7 +5,37 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 import pickle
 import psycopg2
 
-def train_model(parking_ids: list, start_time = datetime.now(), end_time = None, festive_dates = None):
+def get_zone(lat, lon):
+    zones = {
+    "NORD": {
+        "latitude": [46.10, 46.12],
+        "longitude": [11.07, 11.14]
+    },
+    "SUD": {
+        "latitude": [46.04, 46.06],
+        "longitude": [11.07, 11.14]
+    },
+    "EST": {
+        "latitude": [46.06, 46.10],
+        "longitude": [11.11, 11.14]
+    },
+    "OVEST": {
+        "latitude": [46.06, 46.10],
+        "longitude": [11.07, 11.10]
+    },
+    "CENTRO": {
+        "latitude": [46.06, 46.10],
+        "longitude": [11.10, 11.11]
+    }
+    }
+
+    for zone, coords in zones.items():
+        lat_min, lat_max = coords["latitude"]
+        lon_min, lon_max = coords["longitude"]
+        if lat_min <= lat <= lat_max and lon_min <= lon <= lon_max:
+            return zone
+
+def train_model(start_time = datetime.now(), end_time = None, festive_dates = None):
 
     if end_time is None:
         end_time = start_time - timedelta(days=28)
@@ -27,29 +57,28 @@ def train_model(parking_ids: list, start_time = datetime.now(), end_time = None,
     df = pd.DataFrame()
 
     try:
-        for parking_id in parking_ids:
-            query = """
-                SELECT *
-                FROM occupancy_data
-                WHERE parking_id = %s
-                AND timestamp BETWEEN %s AND %s
-            """
-            df_prov = pd.read_sql(query, pg_conn, params=(parking_id, end_time, start_time))
+        query = """
+            SELECT o.*, p.latitude, p.longitude
+            FROM occupancy_data o
+            JOIN parkings p ON o.parking_id = p.parking_id
+            WHERE o.timestamp BETWEEN %s AND %s
+        """
+        df = pd.read_sql(query, pg_conn, params=(end_time, start_time))
 
-            if df_prov.empty:
-                print(f"No data found for parking_id {parking_id}. Skipping...")
-                continue
-            
-            df = pd.concat([df, df_prov])
+        if df.empty:
+            print(f"No data found")      
 
     except Exception as e:
         print(f"Error retrieving parking data: {e}")
 
-    pg_conn.close()
+    finally:
+        pg_conn.close()
 
     df.to_csv('out.csv')
 
     df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df['zone'] = df.apply(lambda row: get_zone(row['latitude'], row['longitude']), axis=1)
+    df.drop(columns=['latitude', 'longitude'], inplace=True)
 
     df['total_capacity'] = df['occupancy'] + df['vacancy']
     df['relative_occupancy'] = df['occupancy'] / df['total_capacity']
@@ -60,6 +89,7 @@ def train_model(parking_ids: list, start_time = datetime.now(), end_time = None,
     df_aggregated = df.groupby(['parking_id', 'rounded_timestamp'], as_index=False).agg(
         mean_occupancy=('relative_occupancy', 'mean')  # Aggregate occupancy
     )
+    df_aggregated['zone'] = df.groupby(['parking_id', 'rounded_timestamp'])['zone'].transform('first')
     df_aggregated.rename(columns={'rounded_timestamp': 'timestamp'}, inplace=True)
 
     # Extract features
@@ -70,9 +100,10 @@ def train_model(parking_ids: list, start_time = datetime.now(), end_time = None,
     df_aggregated['is_festive'] = df_aggregated['timestamp'].dt.date.isin(pd.to_datetime(festive_dates).date) | df_aggregated['is_festive']
 
     # Create dummies
-    df_aggregated['parking_id'] = df_aggregated['parking_id'].astype(str) 
-    parking_dummies = pd.get_dummies(df_aggregated['parking_id'], prefix='parking')
-    df_aggregated = df_aggregated.join(parking_dummies).drop(columns=['parking_id', 'timestamp'])
+    df_aggregated['zone'] = df_aggregated['zone'].astype(str) 
+    parking_dummies = pd.get_dummies(df_aggregated['zone'], prefix='zone')
+    df_aggregated = df_aggregated.join(parking_dummies).drop(columns=['zone', 'timestamp', 'parking_id'])
+    
 
     # Rename occupancy to mean_occupancy
     df_aggregated.rename(columns={'occupancy': 'mean_occupancy'}, inplace=True)
@@ -105,7 +136,7 @@ def train_model(parking_ids: list, start_time = datetime.now(), end_time = None,
         print(f"Error saving model: {e}")
 
 def main():
-    train_model(parking_ids = ["10", "2", "30", "4"])
+    train_model()
 
 
 if __name__ == "__main__":
