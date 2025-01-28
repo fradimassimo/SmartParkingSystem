ffd445ac57e3f12977a890ceb98f5de9a9c2de7f
damianozaccaron <damianozaccaron@gmail.com>
2025-01-28 19:20:15 +0100

    flask try

--- spark_streaming/spark_streaming.py
+++ spark_streaming/spark_streaming.py
@@ -116,8 +116,8 @@ if __name__ == "__main__":
             continue
     # L'unica cosa che mi interessa è che siano (capacity*15) il resto
     sensors = load_json_file('/app/1766_sensors_data.json')
-    str = load_json_file('/app/corrected_closed_parking_structures.json')
-    print(str)
+    str = get_garage_structure()
+    
     if not sensors or not str:
         logger.error("Critical files are missing or invalid. Exiting.")
         exit(1)
