3ab3254cd67f27d274c386f7e929b33ff008d614
damianozaccaron <damianozaccaron@gmail.com>
2025-01-28 21:30:59 +0100

    spark straming sistemato

--- spark_streaming/spark_streaming.py
+++ spark_streaming/spark_streaming.py
@@ -1,129 +1,106 @@
-""""
-How it works:
-Generation of data simulating Spots sensors inside {Park_001, ..., Park_015}.
-Local aggregation, based on coordinates (sensors having same coordinates come from same Park_0**).
-Pub of MQTT messages on topic closed_parking/data in order to consume data in real time on dashboard.
-Saving every 15 minutes in a postgres DB the last snapshot to accumulate data for more accurate predictions.
-
-Spark Streaming could have managed a flux of true realtime data
-I.e. reding a file with a relative path from a remote location,
-such as HFS or S3 and performs data aggregation and processing in an efficient and highly scalable way.
-
-
-[STARTING THE NET]
-docker-compose down --remove-orphans
-docker-compose up --build
-NB, do not run from python file (as long as you just want to test it locally)
+"""
+Connection to Postgres, subscription to an MQTT topic, and saving one message every 15 minutes.
 """
 
-
-from pyspark.sql import SparkSession
 import paho.mqtt.client as mqtt
+import psycopg2
 import json
-from datetime import datetime, timezone
-import time
 import logging
-import random
-from utils import aggregator, get_garage_structure
-
-#spark = SparkSession.builder.appName("SparkStreamingApp").getOrCreate()
+from datetime import datetime, timedelta
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+DB_CONFIG = {
+    "dbname": "smart-parking",
+    "user": "admin",
+    "password": "root",
+    "host": "postgres",
+    "port": "5432"
+}
+
+# Variabile per tracciare l'ultimo messaggio salvato
+last_save_time = datetime.min
+
+
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
-        logger.info("Connected to MQTT Broker successfully")
+        logger.info("Connected to MQTT broker.")
     else:
-        logger.error(f"Failed to connect to MQTT Broker, return code {rc}")
+        logger.error(f"Failed to connect to MQTT broker, return code {rc}")
 
-def on_publish(client, userdata, mid):
-    logger.info(f"Message {mid} has been published.")
 
-def on_log(client, userdata, level, buf):
-    logger.info(f"Log: {buf}")
+def on_message(client, userdata, msg):
+    global last_save_time
 
-def load_json_file(file_path):
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        logger.error(f"File '{file_path}' not found!")
-        return []
-    except json.JSONDecodeError:
-        logger.error(f"Error decoding JSON file '{file_path}'! Ensure it is correctly formatted.")
-        return []
+        logger.info(f"Message received on topic {msg.topic}")
+        data_list = json.loads(msg.payload)
 
-def process_and_publish_data(tempo, lots, structure):
-    try:
+        if not isinstance(data_list, list):
+            logger.error("Received data is not a list.")
+            return
+
+        current_time = datetime.now()
+        # Controlla se sono passati almeno 15 minuti dall'ultimo salvataggio
+        if (current_time - last_save_time) < timedelta(minutes=2):
+            logger.info("Skipping message as 15 minutes haven't passed yet.")
+            return
+
+        # Se sono passati 15 minuti, salva i dati nel database
+        db_conn = userdata["db_conn"]
+        insert_query = """
+            INSERT INTO occupancy_data (parking_id, timestamp, occupancy, vacancy)
+            VALUES (%s, %s, %s, %s)
         """
-        schema = ...
-        df = spark.read.schema(schema).json("/app/live_data.json") 
-        df.createOrReplaceTempView("closed_lots_data")
-        processed_data = ...
-        """
 
-        current_time = tempo
-        record = []
-        for spot in lots:
-            park_flag = random.choice([0, 1])
-            # if the sensor malfunctions, then the spot is marked automatically as occupied
-            battery_percent = 100 if random.random() > 0.005 else 0
-            if battery_percent == 0:
-                park_flag = 0
+        with db_conn.cursor() as cur:
+            for data in data_list:
+                cur.execute(insert_query, (
+                    data["parking_id"],
+                    data["metadata_time"],
+                    data["occupancy"],
+                    data["vacancy"]
+                ))
 
-            single_record = {
-                "device_id": spot["deviceid"],
-                "metadata_time": current_time.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                "location": spot["location"],
-                "payload_fields_park_flag": park_flag,
-                "payload_fields_battery_percent": battery_percent,
-                "payload_fields_low_voltage": "False",
-                "counter": random.randint(1, 1000),
-                "active": 1
-            }
-            record.append(single_record)
-        aggregated = aggregator(structure, record)
+        last_save_time = current_time
+        logger.info(f"Inserted data into DB: {data_list}")
 
-        #for lot in aggregated:
-        try:
-            client.publish("closed_parking/data", json.dumps(aggregated))
-            logger.info(f"Published: {aggregated}")
-        except Exception as error:
-            logger.error(f"Error publishing to MQTT broker: {error}")
     except Exception as e:
-        logger.error(f"Error processing data: {e}")
+        logger.error(f"Error processing message: {e}")
+
+
+def postgres_connection():
+    try:
+        conn = psycopg2.connect(**DB_CONFIG)
+        conn.autocommit = True
+        logger.info("Connected to Postgres DB.")
+        return conn
+    except psycopg2.Error as e:
+        logger.error(f"Error connecting to Postgres DB: {e}")
+        return None
+
 
 if __name__ == "__main__":
-    mqtt_broker = "mosquitto" #mosquitto
+
+    mqtt_broker = "mosquitto"
+    mqtt_topic = "closed_parking/data"
+    db_conn = postgres_connection()
+    if db_conn is None:
+        logger.error("Exiting because Postgres DB connection failed.")
+        exit(1)
 
     client = mqtt.Client()
     client.on_connect = on_connect
-    client.on_publish = on_publish
-    client.on_log = on_log
+    client.on_message = on_message
 
-    retry = 0
-    retry_timeout = 10
-    while retry < 10:
-        try:
-            client.connect(mqtt_broker, 1883, 60)
-            client.loop_start()
-            break
-        except ConnectionRefusedError as error:
-            retry += 1
-            print(f"Connection refused, retrying, attempt: {retry}")
-            time.sleep(retry_timeout)
-            continue
-    # L'unica cosa che mi interessa è che siano (capacity*15) il resto
-    sensors = load_json_file('/app/1766_sensors_data.json')
-    str = get_garage_structure()
-    
-    if not sensors or not str:
-        logger.error("Critical files are missing or invalid. Exiting.")
-        exit(1)
+    # Passa la connessione al database come userdata al client MQTT
+    client.user_data_set({"db_conn": db_conn})
 
-    while True:
-        t = datetime.now(timezone.utc)
-        process_and_publish_data(t, sensors, str)
-        logger.info("Sleeping for 20 seconds")
-        time.sleep(20)
+    try:
+        client.connect(mqtt_broker, 1883, 60)
+        client.subscribe(mqtt_topic)
+        logger.info(f"Subscribed to topic: {mqtt_topic}")
+        client.loop_forever()  # Mantiene la connessione MQTT attiva
+    except Exception as e:
+        logger.error(f"Error in MQTT connection or loop: {e}")
