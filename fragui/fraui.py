37b11352d89d4f77a9c8879efa288b5212411b61
fradimassimo <fradimassimo@icloud.com>
2025-01-28 19:02:31 +0100

    gui

---
+++ fragui/fraui.py
@@ -0,0 +1,122 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+import json
+import paho.mqtt.client as mqtt
+import time
+import json
+import threading
+
+app = Flask(__name__)
+
+# MQTT setup
+
+mqtt_broker = 'mosquitto'
+mqtt_topic_zone = 'zone/select'
+mqtt_topic_alerts = 'closed_parking/data' #(20 sec published)
+
+client = mqtt.Client()
+client.connect(mqtt_broker, 1883, 60)
+
+closed_data = []
+selected_zone = None  # Variabile globale per la zona selezionata
+# Funzione per gestire la selezione della zona da parte dell'utente
+def on_zone_message(client, userdata, msg):
+    global selected_zone
+    try:
+        # Il messaggio contiene la zona selezionata (es. "NORD")
+        zone_requested = msg.payload.decode()  # Decodifica il messaggio ricevuto
+        selected_zone = zone_requested  # Salva la zona selezionata
+
+        print(f"Zona selezionata: {selected_zone}")
+    except Exception as e:
+        print(f"Errore nella gestione della zona: {e}")
+
+
+def get_zone(lat, lon):
+    zones = {
+        "NORD": {"latitude": [46.100001, 46.12], "longitude": [11.070001, 11.14]},
+        "SUD": {"latitude": [46.04, 46.06], "longitude": [11.0070001, 11.14]},
+        "EST": {"latitude": [46.060001, 46.10], "longitude": [11.110001, 11.14]},
+        "OVEST": {"latitude": [46.060001, 46.10], "longitude": [11.070001, 11.10]},
+        "CENTRO": {"latitude": [46.060001, 46.10], "longitude": [11.100001, 11.11]},
+    }
+
+    for zone, coords in zones.items():
+        lat_min, lat_max = coords["latitude"]
+        lon_min, lon_max = coords["longitude"]
+        if lat_min <= lat <= lat_max and lon_min <= lon <= lon_max:
+            return zone
+
+# Funzione per gestire i messaggi di parcheggi chiusi
+def on_closed_message(client, userdata, msg):
+    global closed_data
+    closed_data = []
+    try:
+        closed_parkings = json.loads(msg.payload)
+        print(f"Received alert payload: {closed_parkings}")
+        if isinstance(closed_parkings, list):
+            closed_data = closed_parkings
+        else:
+            print(f"Invalid format or not a list: {closed_parkings}")
+
+    except json.JSONDecodeError as e:
+        print(f"Failed to decode JSON payload: {msg.payload}, error: {e}")
+
+
+# Funzione per ottenere i dati filtrati in base alla zona richiesta
+def get_data_for_zone(zone):
+    """Restituisce i parcheggi relativi alla zona selezionata."""
+    if zone:
+        filtered_parkings = [
+            parking for parking in closed_data
+            if get_zone(parking["location"]["latitude"], parking["location"]["longitude"]) == zone
+        ]
+        return filtered_parkings
+    else:
+        print("Nessuna zona selezionata.")
+        return []
+        # Se non è stata selezionata alcuna zona, restituisci una lista vuota
+
+
+# Subscrizione ai topic
+client.message_callback_add(mqtt_topic_zone, on_zone_message)  # Callback per la selezione della zona
+client.message_callback_add(mqtt_topic_alerts, on_closed_message)  # Callback per i parcheggi chiusi
+
+# Sottoscrizione ai topic
+client.subscribe(mqtt_topic_zone)  # Sottoscrive al topic 'zone/select' per la selezione della zona
+client.subscribe(mqtt_topic_alerts)  # Sottoscrive al topic 'closed_parking/data' per i parcheggi chiusi
+
+def mqtt_loop():
+    client.loop_forever()
+
+mqtt_thread = threading.Thread(target=mqtt_loop)
+mqtt_thread.start()
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/select_zone', methods=["POST"])
+def select_zone():
+    global selected_zone
+    zone = request.form['zone']  # Recupera la zona selezionata dal modulo
+    selected_zone = zone  # Aggiorna la zona selezionata
+    client.publish(mqtt_topic_zone, zone)  # Pubblica la zona selezionata nel topic MQTT
+    print(f"Zona selezionata: {selected_zone}")
+
+    #Redirige l'utente alla pagina corrispondente della zona
+    return redirect(url_for('zone_selected', zone=selected_zone))
+
+
+@app.route('/zone_selected/<zone>')
+def zone_selected(zone):
+    filtered_data = get_data_for_zone(zone)  # Ottieni i parcheggi filtrati per zona
+
+    return render_template('zone_selected.html', zone=zone, parkings=filtered_data)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
